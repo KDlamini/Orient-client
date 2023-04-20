889bdac20176f4ae93cfbb2209ca7dbd70f1720d
Simo Nkosi <wilzmar.kwd@gmail.com>
2023-04-20 12:08:43 +0200

    Add camera rig configuration

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -12,13 +12,13 @@ const CanvasModel = () => {
      <ambientLight intensity={0.5} />
       <Environment preset='city' />
 
-     {/* <CameraRig> */}
+     <CameraRig>
       {/* <Backdrop /> */}
 
       <Center>
         <Shirt />
       </Center>
-     {/* </CameraRig> */}
+     </CameraRig>
     </Canvas>
   )
 }
